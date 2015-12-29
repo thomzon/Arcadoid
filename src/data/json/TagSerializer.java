40a27269f183f5ff4a9b274b0c1337f3d44a1cb8
thomzon <debouverie_thomas@yahoo.fr>
2015-12-29 17:25:24 +0100

    Updated gson library

---
+++ src/data/json/TagSerializer.java
@@ -0,0 +1,13 @@
+package data.json;
+
+import com.google.gson.JsonSerializer;
+
+import data.model.Tag;
+
+public class TagSerializer implements JsonSerializer<Tag> {
+
+	public TagSerializer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
